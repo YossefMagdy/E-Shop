d3f0716794ff52624a2418f2d0f5ff87f71c51b2
YossefMagdy <y.f.magdy@gmail.com>
2024-07-09 00:32:51 +0300

    change api url

--- src/app/Core/enviroment.ts
+++ src/app/Core/enviroment.ts
@@ -1,5 +1,5 @@
 export enum Enviroment {
-    BaseUrlAuth='https://route-ecommerce.onrender.com/api/v1/auth/',
-    BaseUrl='https://route-ecommerce.onrender.com/api/v1/'
+    BaseUrlAuth='https://ecommerce.routemisr.com/api/v1/auth/',
+    BaseUrl='https://ecommerce.routemisr.com/api/v1/'
     // https://route-ecommerce-app.vercel.app/
 }
